f3a72cd80e1bb47f86bca2ecfa9dee9037957fee
eddy <rijanavalona.rakotomalala@gmail.com>
2025-09-09 17:24:19 +0400

    les form insertion

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Book;
 use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,11 +21,19 @@ final class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_book_new', methods: ['GET'])]
-    public function new(): Response
+    #[Route('/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
     {
         $book = new Book();
         $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($book);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_admin_book_index');
+        }
 
         return $this->render('admin/book/new.html.twig', [
             'form' => $form,
