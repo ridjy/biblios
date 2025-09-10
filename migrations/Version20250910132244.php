0a87f2f4f354e6fa5d8bac34f9809e0ca874992d
eddy <rijanavalona.rakotomalala@gmail.com>
2025-09-10 18:14:28 +0400

    creation user et authentification

---
+++ migrations/Version20250910132244.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250910132244 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX uniq_identifier_username');
+        $this->addSql('ALTER TABLE "user" ADD firstname VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD lastname VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD email VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE "user" ALTER username DROP NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_IDENTIFIER_EMAIL');
+        $this->addSql('ALTER TABLE "user" DROP firstname');
+        $this->addSql('ALTER TABLE "user" DROP lastname');
+        $this->addSql('ALTER TABLE "user" DROP email');
+        $this->addSql('ALTER TABLE "user" ALTER username SET NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX uniq_identifier_username ON "user" (username)');
+    }
+}
