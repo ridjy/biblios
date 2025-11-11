3fdf3872807feeb7003e5eeaf394436eeaf684bb
eddy <rijanavalona.rakotomalala@gmail.com>
2025-11-11 11:15:19 +0400

    Demo generate PDF

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,10 +18,17 @@ final class MainController extends AbstractController
     }
 
     #[Route('/pdf', name: 'app_pdf_generate')]
-    public function generatePDF(): Response
+    public function generatePDF(\Knp\Snappy\Pdf $knpSnappyPdf): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $html = $this->renderView('pdf/index.html.twig', array(
+            'some'  => 'var'
+        ));
+
+        return new PdfResponse(
+            $knpSnappyPdf->getOutputFromHtml($html),
+            'file.pdf',
+            'application/pdf',
+            'inline'
+        );
     }
 }
